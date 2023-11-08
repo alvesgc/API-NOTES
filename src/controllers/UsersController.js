b46cecf08f33d40c3cd70905efe552d4eba5db2b
Alisson <alissonalves64@hotmail.com>
2023-11-08 20:20:24 -0300

    :sparkles: feat: implement update at table

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 const AppError = require('../utils/AppError');
 const sqliteConnection = require('../database/sqlite'); 
 class UserController {
@@ -21,21 +21,49 @@ class UserController {
   };
 
   async update(request, response) {
-    const { name , email } = request.body;
+    const { name, email, password, old_password } = request.body;
     const { id } = request.params;
 
     const database = await sqliteConnection();
-    const user = await database.get("SELECT * FROM users WHERE id = (?)", { id });
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
 
     if(!user) {
       throw new AppError("User not found");
     }
 
-    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", { email });
+    const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [ email]);
 
-    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== id ) {
+    if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
       throw new AppError("Email already in use");
     }
+    
+    user.name = name;
+    user.email = email;
+    
+    if(password && !old_password) {
+      throw new AppError("Old password is required");
+    } 
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
+
+      if(!checkOldPassword) {
+        throw new AppError("Old password does not match");
+      }
+    }
+    
+    user.password = await hash(password, 8);
+    await database.run(`
+    UPDATE users SET 
+    name = ?,
+    email = ?,
+    password = ?,
+    updated_at = ? 
+    WHERE id = ?`,
+    [user.name, user.email, user.password, new Date(), id]
+    );
+
+    return response.json();
   }
 }
 

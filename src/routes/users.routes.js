dfaa05fdb19b519d57e54dcaac7762ca49ddc244
Alisson <alissonalves64@hotmail.com>
2023-11-08 20:20:36 -0300

    :sparkles: feat: route to update

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,5 +5,5 @@ const usersRouter = Router();
 const userController = new UserController();
 
 usersRouter.post("/", userController.create);
-
+usersRouter.put("/:id", userController.update)
 module.exports = usersRouter;

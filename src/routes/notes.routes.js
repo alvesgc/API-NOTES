155d5a0521892bffc482aea4e2a4bd70c98022f9
Alisson <alissonalves64@hotmail.com>
2023-11-13 19:23:34 -0300

    :sparkles: feat:  Método Get para exibir dados.

---
+++ src/routes/notes.routes.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+const NotesController = require("../controllers/NotesController")
+const notesRouter = Router();
+
+const notesController = new NotesController();
+
+notesRouter.post("/:user_id", notesController.create);
+notesRouter.get("/:id", notesController.show);
+module.exports = notesRouter;

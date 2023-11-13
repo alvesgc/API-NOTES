39a2e6e3982baccec60cdf2247a66910020180b0
Alisson <alissonalves64@hotmail.com>
2023-11-13 19:30:12 -0300

    ✨ feat: Delete método para deletar em cascata

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -6,4 +6,5 @@ const notesController = new NotesController();
 
 notesRouter.post("/:user_id", notesController.create);
 notesRouter.get("/:id", notesController.show);
+notesRouter.delete("/:id", notesController.delete);
 module.exports = notesRouter;

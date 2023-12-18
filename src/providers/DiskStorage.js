23e7c021c55fbfe3e5b7bbad4814d75ab267b718
Alisson <alissonalves64@hotmail.com>
2023-12-18 20:11:36 -0300

    feat: save and delete images in storage.

---
+++ src/providers/DiskStorage.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const path = require('path');
+const uploadConfig = require('../config/upload');
+
+class DiskStorage { 
+  async saveFile(file) {
+    await fs.promises.rename(
+      path.resolve(uploadConfig.TMP_FOLDER, file),
+      path.resolve(uploadConfig.UPLOADS_FOLDER, file)
+    );
+    return file;
+  }
+
+  async deleteFile(file){
+    const filepath = path.resolve(uploadConfig.UPLOADS_FOLDER, file);
+    try {
+      await fs.promises.stat(filepath);
+    }catch {
+        return
+    }
+    await fs.promises.unlink(filepath);
+  }
+};
+
+module.exports = DiskStorage;

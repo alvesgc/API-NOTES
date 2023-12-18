c95a357f73235c9f58cd25adfb778cfcfa7a31ec
Alisson <alissonalves64@hotmail.com>
2023-12-18 20:36:22 -0300

    refactor: JWT to authenticate routes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -1,11 +1,13 @@
 const { Router } = require("express");
 const NotesController = require("../controllers/NotesController")
 const notesRouter = Router();
-
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 const notesController = new NotesController();
 
+notesRouter.use(ensureAuthenticated);
+
 notesRouter.get("/", notesController.index);
-notesRouter.post("/:user_id", notesController.create);
+notesRouter.post("/", notesController.create);
 notesRouter.get("/:id", notesController.show);
 notesRouter.delete("/:id", notesController.delete);
 module.exports = notesRouter;
--- src/routes/tags.routes.js
+++ src/routes/tags.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const TagsController = require("../controllers/TagsController")
 const tagsRouter = Router();
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated")
 
 const tagsController = new TagsController();
 
-tagsRouter.get("/:user_id", tagsController.index);
+tagsRouter.get("/", ensureAuthenticated, tagsController.index);
 module.exports = tagsRouter;
